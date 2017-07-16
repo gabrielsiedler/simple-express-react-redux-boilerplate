7c1fef2f7bf8887e91f376092c979145bbeb8b1d
Gabriel Siedler <gabrielsiedler@gmail.com>
2017-07-16 09:52:37 -0300

    Small organization changes

---
+++ src/app/components/ui/index.js
@@ -0,0 +1,17 @@
+import Brand from './Brand';
+import CollapseNavbar from './CollapseNavbar';
+import Container from './Container';
+import Loader from './Loader';
+import MenuLink from './MenuLink';
+import Navbar from './Navbar';
+import Ul from './Ul';
+
+export {
+  Brand,
+  CollapseNavbar,
+  Container,
+  MenuLink,
+  Navbar,
+  Loader,
+  Ul,
+};
